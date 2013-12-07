80f2515a3a06f6b84052db4bdcdd466dc19a7ad4
MoonGuard <ufamoonguard@gmail.com>
2013-12-07 14:49:08 +0600

    fixed: lagrange2

--- LibraryCourseWork/LibraryCourseWork/Lagange2.cs
+++ LibraryCourseWork/LibraryCourseWork/Lagange2.cs
@@ -12,50 +12,59 @@ namespace LibraryCourseWork
     public class Lagange2
     {
         /// <summary>
-        /// Тут теперь все хитрее, нам нужно не просто трехмерное про-во
-        /// А мы задаем области, т.е. прямоугольную сетку значений NxM
+        /// мы задаем области, т.е. прямоугольную сетку значений NxM
         /// </summary>
-        private double[,] x;
-        private double[,] y;
-        public Lagange2(double[,] X,double[,] Y)
+        private double[] _x;
+        private double[] _y;
+        //Наши значения в точке f(x[i],y[j]);
+        private double[,] _f;
+        //n - size(x), m - size(y)
+        int n, m;
+        public Lagange2(double[] x,double[] y, double[,] f)
         {
-            x = new double[X.GetLength(0), X.GetLength(1)];
-            y = new double[X.GetLength(0), X.GetLength(1)];
-            for (int i = 0; i < X.GetLength(0); i++)
+            n = x.Length;
+            m = y.Length;
+            _x = new double[n];
+            _y = new double[m];
+            _f = new double[n,m];
+            if (f.GetLength(0) != n || f.GetLength(1) != m)
+                throw new Exception("Не хватает кол-ва значений для узлов, метод Лагранжа не применим");
+            for (int i = 0; i < n; ++i)
             {
-                for (int j = 0; j < X.GetLength(1); j++)
-                {
-                    x[i,j]= X[i,j];
-                    y[i, j] = Y[i, j];
-                }
+                _x[i] = x[i];
             }
+            for (int i = 0; i < m; ++i)
+            {
+                _y[i] = y[i];
+            }
+            for(int i = 0; i<n;++i)
+                for (int j = 0; j < m; ++j)
+                {
+                    _f[i, j] = f[i, j];
+                }
             //name = "Лагранж от двух переменных";
         }
 
-        public double Calc(double x0,double y0)
+        public double Calc(double x0, double y0)
         {
-            int n = x.GetLength(0);
-            int m = x.GetLength(1);
-            //Сюда мы будем заносить одну строку массива
-            double[] str = new double[m];
-            //Сюда мы будем записывать значение для каждого узла в строчке
-            double[] str2 = new double[m];
-            //Массив для интерполяции по У
-            double[] tmp = new double[n];
-            /*Интерполяция по X*/
-            for (int i = 0; i < n; i++)
-            {
-                for (int j = 0; j < m; j++)
+            double lagrange = 0;
+            //Для учета первого произведения
+            double basicPolynomial = 1;
+            for(int i=0;i<n;++i)
+                for (int j = 0; j < m; ++j)
                 {
-                    str[j] = x[i, j];
-                    str2[j] = y[i,j];
+                    basicPolynomial = 1;
+                    for (int k = 0; k < n; ++k)
+                    {
+                        for (int t = 0; t < m; ++t)
+                        {
+                            if ((t == j) || (k == i)) continue;
+                            basicPolynomial *= (((x0 - _x[k]) * (y0 - _y[t])) / ((_x[i] - _x[k]) * (_y[j] - _y[t])));
+                        }
+                    }
+                    lagrange += basicPolynomial * _f[i,j];
                 }
-                Lagrange lg = new Lagrange(str, str2);
-                tmp[i] = lg.Calc(x0);
-            }
-            /*Интерполяция по Y*/
-            Lagrange lg2 = new Lagrange(str, tmp);
-            return lg2.Calc(y0);
+            return lagrange;
         }
     }
 }
